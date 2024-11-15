f38dc0c652523455303f1fc38e9c6a69d4a3a38f
wilsontomas <wilsonpumm@gmail.com>
2024-11-15 13:47:00 -0400

    material and home compoennt added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component:HomeComponent,
+        path:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'**',
+        redirectTo:'home', 
+    }
+];
